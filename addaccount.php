be73e67422b2b2fff3006d5ac1b96f5970de9199
amyguobunny <amyguo@umich.edu>
2015-12-02 14:36:21 -0500

    signUpNewAccount

---
+++ addaccount.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <title>Sign up</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="container text-center">
+    <p>Welcome<?php echo $_POST["fName"];?></p>
+    <p>You have created an account successfully!</p>
+  </div>
+</body>
+</html>
